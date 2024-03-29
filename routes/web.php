a4157c8ee80e3d2ee1e768e6e27295c86698c848
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-03-29 18:27:55 +0700

    change1

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\PendidikanController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Route::get('/jabatan', [JabatanController::class, 'index']);
+
+Route::get('/pendidikan', [PendidikanController::class, 'index']);
